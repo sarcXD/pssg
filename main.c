7b598ada60155e89dbbbd589e808b55d4e0aa334
Talha Aamir <talhaaamirwork@gmail.com>
2022-03-20 02:44:56 +0500

    Done: Debug Info generation on compilation and linking WIP: Reading .pssg files

--- main.c
+++ main.c
@@ -7,24 +7,20 @@
 #define false 0
 #define MToKB(mb) (mb*1024)
 #define MToB(mb) (mb*1024*1024)
-#define STR_MAXLEN 64
-#define FILE_MAXLEN 64
+
+#define STR_MAXLEN 256
+#define FILE_MAXLEN 256
+#define MAX_PSSG_FILES 50
+#define BLOG_FILE_LEN 2048
+#define GIT_IGN_MAXLEN 32
+
 #define ALLOCSIZE MToB(64)
+
 char *Buffer;
 char *allocp;
 struct ProgState{
   char *configPath; // .ppsg files path
-  char *ignorefile; // .gitignore file
-};
-
-struct configState {
-  /*
-    how this is organized: 
-    char name -> File data, 
-    char name -> File data,
-    ...
-  */
-  char *obj;
+  KV_Pair *LookupTable; // pssg files store in a basic lookup table
 };
 
 // Utility Functions
@@ -34,7 +30,7 @@ struct configState {
  * @param ptr pointer to allocate space to
  * @param reqSz size we need to allocate
  */
-char *p_stalloc_ch(int reqSz) {
+void *p_stalloc_ch(int reqSz) {
   if (Buffer + ALLOCSIZE - allocp >= reqSz) {
     allocp+=reqSz;
     return allocp-reqSz;
@@ -48,7 +44,7 @@ char *p_stalloc_ch(int reqSz) {
  * @param ptr the pointer we want to free
  * @param sz the size of the pointer to free
  */
-void p_stfree_ch(char *ptr) {
+void p_stfree_ch(void *ptr) {
   if (ptr >= Buffer && ptr <= Buffer + ALLOCSIZE) {
     allocp = ptr;
   }
@@ -61,10 +57,16 @@ void SearchInPath(char *fileDir, char *ignoreFile, int numl, struct ProgState st
   HANDLE hFind;
   LARGE_INTEGER filesize;
   DWORD dwError=0;
+  
   // 1. update the fileDir with /*
   char *wfileDir=p_stalloc_ch(STR_MAXLEN);
   strcpy(wfileDir,fileDir);
-  strcat(wfileDir, (char *)("/*"));
+  int fdLen = strlen(wfileDir) -1;
+  // windows requires dir path have a trailing slash
+  // we add that automatically, as users can conventionally add both
+  bool dirPadding = wfileDir[fdLen] == '/' || wfileDir[fdLen] == '\\' ? false : true;
+  dirPadding ? strcat(wfileDir, (char *)("/*")):strcat(wfileDir, (char *)("*"));
+  
   // 2. Read file
   hFind = FindFirstFile(wfileDir, &ffd);
   if (hFind == INVALID_HANDLE_VALUE) 
@@ -78,28 +80,27 @@ void SearchInPath(char *fileDir, char *ignoreFile, int numl, struct ProgState st
     if (p_strcmp(ffd.cFileName,".") || p_strcmp(ffd.cFileName, "..")) continue;
     // ..checking entries with ignore file
     if (p_strcmpMulti(ffd.cFileName,ignoreFile, numl, STR_MAXLEN)) continue;
-    // if the dir is the pssg config
-    if (p_strcmp(ffd.cFileName,".pssg")) {
-      strcpy(state.configPath, fileDir);
-      strcat(state.configPath,"/.pssg");
-      continue;
-    }
     if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
     {
-        printf("  %s   <DIR>\n", ffd.cFileName);
+        printf("%s   <DIR>\n", ffd.cFileName);
         char *tempDir = p_stalloc_ch(STR_MAXLEN);
         strcpy(tempDir, fileDir);
-        strcat(tempDir,"/");
-        strcat(tempDir,ffd.cFileName);
+        if (dirPadding) strcat(tempDir,"/");
+        strcat(tempDir, ffd.cFileName);
         SearchInPath(tempDir, ignoreFile, numl, state);
         p_stfree_ch(tempDir);
+        continue;
     }
-    else {
-        filesize.LowPart = ffd.nFileSizeLow;
-        filesize.HighPart = ffd.nFileSizeHigh;
-        p_substrcmp(ffd.cFileName,".html") ? /*readFile*/ printf("true") : printf("false");
-        printf("  %s   %lld bytes\n", ffd.cFileName, filesize.QuadPart);
+    filesize.LowPart = ffd.nFileSizeLow;
+    filesize.HighPart = ffd.nFileSizeHigh;
+    if (!p_substrcmp(ffd.cFileName,".html")) { // file is not html
+      continue;
     }
+    if (p_substrcmp(fileDir,".pssg")) {
+      // load into state LookupTable
+      printf("This is pssg file %s",ffd.cFileName);
+    }
+    printf("|  %s   %lld bytes\n", ffd.cFileName, filesize.QuadPart);
   }
   while (FindNextFile(hFind, &ffd) != 0);
   dwError = GetLastError();
@@ -112,17 +113,19 @@ void SearchAndReplace(char *fileDir) {
   Buffer = (char *) malloc (ALLOCSIZE);
   allocp = Buffer;
   struct ProgState state;
-  state.configPath = p_stalloc_ch(STR_MAXLEN); 
-  char *fileReadBuff = p_stalloc_ch(STR_MAXLEN*FILE_MAXLEN);
+  state.configPath = p_stalloc_ch(STR_MAXLEN);
+  char *fileReadBuff = p_stalloc_ch(STR_MAXLEN*GIT_IGN_MAXLEN);
+  state.LookupTable = p_stalloc_ch(STR_MAXLEN*FILE_MAXLEN*MAX_PSSG_FILES); // 3.12~ mb
   char *ignorefpath = p_stalloc_ch(STR_MAXLEN);
   strcpy(ignorefpath, fileDir);
-  strcat(ignorefpath,"\\.gitignore");
-  printf(ignorefpath);
+  strcat(ignorefpath,"/.gitignore");
+  printf("%s \n",ignorefpath);
   int numl = readFile(ignorefpath, fileReadBuff, "r", STR_MAXLEN);
   p_stfree_ch(ignorefpath);
   SearchInPath(fileDir, fileReadBuff, numl, state);
   p_stfree_ch(fileReadBuff);
   p_stfree_ch(state.configPath);
+  free(Buffer);
 }
 
 int main( int argc, char *argv[]) {
@@ -136,15 +139,15 @@ int main( int argc, char *argv[]) {
         }
         case 'h': {
           printf("PSSG: The Bloat free, static site generator (helper)\n");
-          printf("use: main.exe -[arguments]\n");
-          printf("arguments             Description\n");
-          printf("-e {$project folder}  Execute Custom html replacement operation. This will find any and all custom elements\n\
-                      in your static site that are marked as follows:\n\
-                      `.obj\n\
-                      <Navbar />`\n\
-                      The Program then searches in a .pssg\\objects folder in your root directory.\n\
-                      If it finds a Navbar.html file, it will replace all such entries in your static site html\n\
-                      With this custom html");
+          printf("use: pssg.exe -[arguments]\n");
+          printf("arguments\t\tDescription\n");
+          printf("-e {$project folder}\tExecute Custom html replacement operation. This will find any and all custom elements\n\
+                \tin your static site that are marked as follows:\n\
+                \t`.obj\n\
+                \t<Navbar />`\n\
+                \tThe Program then searches in a .pssg\\objects folder in your root directory.\n\
+                \tIf it finds a Navbar.html file, it will replace all such entries in your static site html\n\
+                \tWith this custom html");
           return 1;
         }
         default: {
